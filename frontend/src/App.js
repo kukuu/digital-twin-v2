366ce7641ffacbec07be610e445b355051fbd7f3
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2025-01-18 06:20:38 +0000

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -69,7 +69,7 @@ export default function EnergyMeter() {
     <div className="container">
       <h1 className="title">Digital Twin - Live Energy Meter Readings</h1>
 
-  <p>At DSS Digital Smart Meter Centre, we help you find the best electricity meter 
+  <p>The DSS <strong>Digital Smart Meter</strong>, helps you find the best electricity meter 
     at the most competitive price. Compare diferent meters, check prices and choose the right option to save on energy bills. </p>
         
        <p> Start comparing now and make smarter choices for 
