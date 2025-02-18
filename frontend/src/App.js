adbc467c0a002d7815d8375294f95b2089288a0b
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2025-02-18 14:11:08 +0000

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -68,7 +68,7 @@ export default function EnergyMeter() {
   return (
     <div className="container">
       <h1 className="title">SPYDER</h1>
-    <p><strong><center>Price Comaprison Smart Energy Meter Reader</center></strong></p>
+    <p className="intro">Price Comaprison Smart Energy Meter Reader</p>
 
   <p>The <strong>SPYDER</strong> Digital Twin Smart Energy Meter Reader, helps you find the best electricity meter 
     at the most competitive price. Compare diferent meters, check prices and choose the right option to save on energy bills. </p>
