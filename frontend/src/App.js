9dff3897c6cfa4464eceb11d50d0abd80732cfb0
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2025-06-06 21:13:37 +0100

    Update App.js
    Updated prompt to change reading.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -322,7 +322,7 @@ export default function EnergyMeter() {
             <div className="reading-display">
               <span>{data.reading}</span>
               <span className="unit">kWh</span>
-              <p className="changeReading">Change reading</p>
+              <p className="changeReading">Use my reading</p>
             </div>
             <div className="other-display">
               <span className="unit2">Supplier:</span>
