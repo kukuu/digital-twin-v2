f57713a711daecb69bd25a8fa81582028e8c2ce5
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2025-06-18 17:24:35 +0100

    Update index.js
    Added routes.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import AdvertisingPage from "./advertising/AdvertisingPage"; // Add this import
+import NewsletterPage from "./newsletter/NewsletterPage"; // Add this import
 import { ClerkProvider } from "@clerk/clerk-react";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter, Routes, Route } from 'react-router-dom'; // Add Routes and Route
@@ -21,6 +22,7 @@ root.render(
         <Routes>
           <Route path="/" element={<App />} />
           <Route path="/advertising" element={<AdvertisingPage />} />
+          <Route path="/newsletter" element={<NewsletterPage />} />
         </Routes>
       </ClerkProvider>
     </BrowserRouter>
