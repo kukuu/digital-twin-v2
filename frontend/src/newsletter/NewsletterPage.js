e26fdcec27d6d68cd0ca9d44275f1c488a59d643
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2025-06-26 17:26:30 +0100

    Update NewsletterPage.js

--- frontend/src/newsletter/NewsletterPage.js
+++ frontend/src/newsletter/NewsletterPage.js
@@ -4,8 +4,10 @@ import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 import { SignedIn, SignedOut, SignInButton, UserButton, useUser } from "@clerk/clerk-react";
+import { Link } from 'react-router-dom';
 import "./Newsletter.css";
 import "../App.css";
+import { ToastContainer, toast } from "react-toastify";
 
 
 export default function NewsletterPage() {
@@ -24,12 +26,15 @@ export default function NewsletterPage() {
 
   
   return (
-    <div className="advertising-container">
-
-
-     <nav className="navbar">
+    <div className="app-container">
+      {/* Navigation */}
+      <ToastContainer position="top-right" autoClose={3000} />
+      <nav className="navbar">
         <div className="navbar-brand">
           <h2 style={{ fontWeight: "bold", color: "green" }}><a href="/">SPYDER</a></h2>
+          <Link to="/newsletter" className="crumbtrail"><small>Newsletter | </small></Link> 
+          <Link to="/advertising" className="crumbtrail"> <small>Advertising | </small></Link>
+          <Link to="/pricing" className="crumbtrail"> <small>Pricing</small></Link>
         </div>
         <div className="navbar-auth">
           <SignedOut>
@@ -42,10 +47,13 @@ export default function NewsletterPage() {
       </nav>
 
       <div className="advertising-header">
-        <h3>Newsletter</h3>
-        <p>   The <strong>SPYDER</strong> Newsletter ......</p>
+        <h1>Newsletter</h1>
       </div>
-
+      <div className="main-content">
+          <p className="header-description">
+              More content...
+            </p>
+      </div>
     </div>
   );
 }
