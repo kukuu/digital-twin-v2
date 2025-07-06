1feba8ec0239592a62a71f95fecd5cd54f0f1b92
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2025-07-06 16:07:06 +0100

    Update NewsletterPage.js
    Updated Crumbtrail alignment.

--- frontend/src/newsletter/NewsletterPage.js
+++ frontend/src/newsletter/NewsletterPage.js
@@ -32,9 +32,6 @@ export default function NewsletterPage() {
       <nav className="navbar">
         <div className="navbar-brand">
           <h2 style={{ fontWeight: "bold", color: "green" }}><a href="/">SPYDER</a></h2>
-          <Link to="/newsletter" className="crumbtrail"><small>Newsletter | </small></Link> 
-          <Link to="/advertising" className="crumbtrail"> <small>Advertising | </small></Link>
-          <Link to="/pricing" className="crumbtrail"> <small>Pricing</small></Link>
         </div>
         <div className="navbar-auth">
           <SignedOut>
@@ -43,6 +40,11 @@ export default function NewsletterPage() {
           <SignedIn>
             <UserButton afterSignOutUrl="/" />
           </SignedIn>
+        </div>
+         <div>
+          <Link to="/newsletter" className="crumbtrail"><small>Newsletter | </small></Link> 
+          <Link to="/advertising" className="crumbtrail"> <small>Advertising | </small></Link>
+          <Link to="/pricing" className="crumbtrail"> <small>Pricing</small></Link>
         </div>
       </nav>
 
