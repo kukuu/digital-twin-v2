fc0f10d499e5708a225429ffafa2f24251d23ac5
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2025-07-21 09:32:39 +0100

    Update PricingPage.js

--- frontend/src/pricing/PricingPage.js
+++ frontend/src/pricing/PricingPage.js
@@ -1,5 +1,3 @@
-"use client";
-
 import React, { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
